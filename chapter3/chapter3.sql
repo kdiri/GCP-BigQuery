d8db0b7035a850c52e15dc36480286b3bc40188d
Kemal Diri <kemaldiri@gmail.com>
2021-02-25 09:59:51 +0100

    :lion_face: Enjoy with chapter 3

--- chapter3/chapter3.sql
+++ chapter3/chapter3.sql
@@ -22,3 +22,35 @@ select *, round(oneways / numrides, 2) as frac_oneway from example
 -- Sun	2376	936	        0.39
 
 
+-- Safe division by zero
+with example as (
+    select 'Sat' as day, 1451 as numrides, 1018 as oneways
+    union all select 'Sun', 2376, 936
+    union all select 'Wed', 0, 0
+)
+select *, ROUND(IEEE_Divide(oneways, numrides), 2) as frac_oneway from example
+
+-- day	numrides	oneways	frac_oneway
+-- Sat	1451	    1018	0.7
+-- Sun	2376	    936	    0.39
+-- Wed	0	        0	    NaN
+
+-- SAFE functions
+-- We can make scalar function return 0 instead of raising an error.
+select log(10, -3)
+-- become
+select safe.log(10, -3)
+
+
+-- Safe division and filtering
+with example as (
+    select 'Sat' as day, 1451 as numrides, 1018 as oneways
+    union all select 'Sun', 2376, 936
+    union all select 'Tue', IEEE_Divide(-3, 0), 0
+)
+select * from example
+where numrides < 2000
+
+-- day	numrides	oneways
+-- Sat	1451.0	    1018
+-- Tue	-Infinity	0
