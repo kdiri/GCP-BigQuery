586992633eb5d79f8ed6eaf1a97592b11dc39cae
Kemal Diri <kemaldiri@gmail.com>
2021-03-02 10:23:11 +0100

    timestamps

--- chapter3/chapter3.sql
+++ chapter3/chapter3.sql
@@ -120,5 +120,91 @@ from example
 -- 2
 
 -- BUT IF approach is better that casting (performance)
+-- Same results
+with example as (
+    select true as is_vowel, 'a' as letter, 1 as position,
+    union all select false, 'b', 2
+    union all select false, 'c', 3
+    union all select false, 'd', 4
+    union all select true, 'e', 5
+)
+select countif(is_vowel) as num_vowels
+from example
+
+-- Some STR functions
+with example as (
+    select * from unnest([
+        'New York', 'Seattle', 'Madrid', 'Singapore'
+    ]) as city
+)
+select
+    city
+    , length(city) as len
+    , lower(city) as low
+    , strpos(city, 'or') -- find the position of given str at right
+from example
+
+-- city	     len	low	        f0_
+-- New York	 8	    new york	6
+-- Seattle	 7	    seattle	    0
+-- Madrid	 6	    madrid	    0
+-- Singapore 9	    singapore	7
+
+WITH email_addresses AS
+  (SELECT
+    "foo@example.com" AS email_address
+  UNION ALL
+  SELECT
+    "foobar@example.com" AS email_address
+  UNION ALL
+  SELECT
+    "foobarbaz@example.com" AS email_address
+  UNION ALL
+  SELECT
+    "quxexample.com" AS email_address)
+
+SELECT
+  STRPOS(email_address, "@") AS example
+FROM email_addresses;
+
+-- example
+-- 4
+-- 7
+-- 10
+-- 0
+
+
+-- Some cool things
+with example as (
+    select 'abc@email.com' as email, 'Minneapolis' as city
+    union all select 'reno@abc.com', 'Chicago'
+    union all select 'keke@keko.com', 'Medford'
+)
+select
+    concat(
+        substr(email, 1, STRPOS(email, '@') -1 ), -- username
+        ' from ', city
+    ) as callers
+from example
+
+-- callers
+-- abc from Minneapolis
+-- reno from Chicago
+-- keke from Medford
+
+
+-- A timestamp represents an absolute point in time regardless of location.
+-- Thus a timestamp of 2021-03-02 07:30:00.45 (Mar 02, 2021, at 07:30 UTC)
+-- represents the same time as 2021-03-02 08:30:00.45+1:00 (8:30 a.m. at a time zone that is an hour behind):
+
+select t1, t2, timestamp_diff(t1, t2, microsecond)
+from (
+    select
+    timestamp "2021-03-02 07:30:00.45" as t1,
+    timestamp "2021-03-02 08:30:00.45+1" as t2
+)
+
+-- t1	                        t2	                         f0_
+-- 2021-03-02 07:30:00.450 UTC	2021-03-02 07:30:00.450 UTC	 0
 
 
